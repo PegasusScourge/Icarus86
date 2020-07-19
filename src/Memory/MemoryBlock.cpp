ed5ecfc68e9ff0a18fcb84e8e91e860fb105970b
PegasusScourge <soniduino@gmail.com>
2020-07-19 19:30:59 +0100

    Added MemoryBlock

---
+++ src/Memory/MemoryBlock.cpp
@@ -0,0 +1,48 @@
+/**************************************************************************
+
+MemoryBlock.cpp
+
+Created by Matthew Clarke
+
+Controls a block of memory that the CPU can access
+
+ **************************************************************************/
+
+#include "MemoryBlock.hpp"
+
+namespace im = icarus::memory;
+
+im::MemoryBlock::MemoryBlock(size_t startAddress, size_t size) {
+	m_data = new uint8_t[size];
+	m_size = size;
+	m_startAddress = startAddress;
+}
+
+im::MemoryBlock::~MemoryBlock() {
+	delete[] m_data;
+}
+
+bool im::MemoryBlock::isInRange(size_t address) {
+	return address >= m_startAddress && address < (m_startAddress + m_size);
+}
+
+void im::MemoryBlock::tryByteWrite(size_t address, uint8_t value) {
+	if (isInRange(address))
+		m_data[address - m_startAddress] = value;
+}
+
+bool im::MemoryBlock::tryByteRead(size_t address, uint8_t* dest) {
+	if (!isInRange(address) || dest == nullptr)
+		return false;
+
+	*dest = m_data[address - m_startAddress];
+	return true;
+}
+
+size_t im::MemoryBlock::getSize() {
+	return m_size;
+}
+
+size_t im::MemoryBlock::getStartAddress() {
+	return m_startAddress;
+}
---
+++ src/Memory/MemoryBlock.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+/**************************************************************************
+
+MemoryBlock.hpp
+
+Created by Matthew Clarke
+
+Controls a block of memory that the CPU can access
+
+ **************************************************************************/
+
+#include "../Type.hpp"
+
+namespace icarus {
+
+	namespace memory {
+
+		class MemoryBlock {
+		private:
+			uint8_t* m_data;
+			size_t m_startAddress = 0;
+			size_t m_size;
+
+			bool isInRange(size_t address);
+
+		public:
+			MemoryBlock(size_t startAddress, size_t size);
+			~MemoryBlock();
+
+			void tryByteWrite(size_t address, uint8_t value);
+			bool tryByteRead(size_t address, uint8_t* dest);
+
+			size_t getSize();
+			size_t getStartAddress();
+
+		};
+
+	}
+
+}
